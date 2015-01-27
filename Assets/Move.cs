6bf12e87a673aa9caddd1bca15f4924ef8c7688e
Paul Sinnett <paul.sinnett@gmail.com>
2015-01-27 15:21:20 +0000

    initial commit

---
+++ Assets/Move.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour
+{
+		public Transform target;
+		public bool on;
+
+		void Start ()
+		{
+			transform.position = target.position;
+		}
+
+		void Update ()
+		{
+		}
+}
+
+
