22bb6d824498fb6b5d04b155eb652414d083e27e
Paul Sinnett <paul.sinnett@gmail.com>
2015-02-04 19:31:04 +0000

    tidy up

--- Assets/Move.cs
+++ Assets/Move.cs
@@ -4,16 +4,9 @@ using System.Collections;
 public class Move : MonoBehaviour
 {
 		public Transform target;
-		public bool on;
 
 		void Start ()
 		{
 			transform.position = target.position;
 		}
-
-		void Update ()
-		{
-		}
 }
-
-
